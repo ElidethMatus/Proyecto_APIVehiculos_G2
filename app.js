a79eef079ff40151fa0e0074df524ae8e99087c6
ElidethMatus <elidethmatus@gmail.com>
2026-03-19 21:59:04 -0600

    borré un archivo que estaba de más

--- app.js
+++ app.js
@@ -1,35 +1,4 @@
 const express = require('express');
-<<<<<<< HEAD
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-require('dotenv').config();
-
-const app = express();
-app.use(express.json());
-
-const PORT = process.env.PORT || 3000;
-const SECRET_KEY = process.env.JWT_SECRET_KEY;
-const pool = require('./config/db');
-const AuthMiddleware = require('./middleware/authMiddleware');
-
-
-const authRoute = require('./routes/authRoute');
-const usuariosRoutes = require('./routes/usuariosRoutes');
-const vehiculosRoutes = require('./routes/vehiculosRoutes');
-
-app.use('/api/auth', authRoute);
-app.use('/api/usuarios', usuariosRoutes);
-app.use('/api/vehiculos', vehiculosRoutes);
-
-app.get('/', (req, res) => {
-    res.send('La API está escuchando');
-});
-
-
-
-app.listen(PORT, () => {
-    console.log(`Servidor escuchando en http://localhost:${PORT}`);
-=======
 const mysql = require('mysql2');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
@@ -51,5 +20,4 @@ const SECRET_KEY = process.env.JWT_SECRET_KEY;
 
 app.listen(PORT,()=>{
   console.log(`El servidor esta escuchando en en http://localhost:${PORT}`);
->>>>>>> origin/develop
 });
